716764f37f86e337e5127cfac092288d33b7a087
Pritam Paul <pritampaul.10000@gmail.com>
2026-08-23 13:03:59 +0530

    Time: 8 ms (53.28%), Space: 46.9 MB (83.94%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int n = num.length();
+
+        int s1 = 0;
+        int s2 = 0;
+
+        int q1 = 0;
+        int q2 = 0;
+
+        // Left half
+        for (int i = 0; i < n / 2; i++) {
+            if (num.charAt(i) == '?') {
+                q1++;
+            } else {
+                s1 += num.charAt(i) - '0';
+            }
+        }
+
+        // Right half
+        for (int i = n / 2; i < n; i++) {
+            if (num.charAt(i) == '?') {
+                q2++;
+            } else {
+                s2 += num.charAt(i) - '0';
+            }
+        }
+
+        // Odd number of '?' means Alice gets the last move
+        if ((q1 + q2) % 2 == 1) {
+            return true;
+        }
+
+        return s1 - s2 != 9 * (q2 - q1) / 2;
+    }
+}
